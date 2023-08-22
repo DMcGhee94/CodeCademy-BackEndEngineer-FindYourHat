93657da63f873fda9d5d23bb4202a87351bc19ee
Darren McGhee <>
2023-08-22 22:20:37 +0100

    Completion of steps 1-3

--- starting/main.js
+++ starting/main.js
@@ -4,3 +4,23 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+    constructor(field) {
+        this._field = field;
+    }
+
+    print() {
+        console.log(this._field);
+    };
+};
+
+const myField = new Field(
+    [
+        ['*', '░', 'O'],
+        ['░', 'O', '░'],
+        ['░', '^', '░'],
+    ]
+);
+
+myField.print();
